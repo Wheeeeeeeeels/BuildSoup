d0b95960be15635c566e048d21a997a2c97ca607
Wheeeeeeeeels <ada_wheels@163.com>
2022-08-23 02:54:24 -0400

    Update FFMpeg_x264_x265_aac_opus.sh

--- FFMpeg/FFMpeg_x264_x265_aac_opus.sh
+++ FFMpeg/FFMpeg_x264_x265_aac_opus.sh
@@ -38,8 +38,8 @@ mkdir ffmpeg_build
 else echo "The Folder already exists"
 fi
 # ffmpeg_bin: Used to generated binary files, including ffmpeg, ffprobe, x264, x265
-if [ ! -d "/ffmpeg_bin/" ];then
-mkdir ffmpeg_bin
+if [ ! -d "/bin/" ];then
+mkdir bin
 else echo "The Folder already exists"
 fi
 
@@ -57,7 +57,7 @@ sudo sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
 sudo sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
 
 # 4. Install the base dependency
-sudo yum -y install autoconf automake bzip2 bzip2-devel cmake freetype-devel gcc gcc-c++ git libtool make mercurial pkgconfig zlib-devel
+sudo yum -y install autoconf automake bzip2 bzip2-devel cmake freetype-devel gcc gcc-c++ git libtool make mercurial pkgconfig nasm zlib-devel
 
 echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>2. The Base Dependency Success.>>>>>>>>>>>>>>>>>>>>>>>>>"
 
@@ -72,11 +72,9 @@ tar xjvf nasm-2.15.05.tar.bz2
 echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>3.2 The nasm decompression Success.>>>>>>>>>>>>>>>>>>>>>"
 
 cd nasm-2.15.05
-sudo sh autogen.sh
-
+sudo bash autogen.sh
 echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>3.3 The nasm build start>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-./configure --prefix="$HOME_DIRECTORY/ffmpeg_build" --bindir="$HOME_DIRECTORY/ffmpeg_bin"
-
+./configure --prefix="$HOME_DIRECTORY/ffmpeg_build" --bindir="$HOME_DIRECTORY/bin"
 
 sudo make -j4
 sudo make install
@@ -96,7 +94,7 @@ tar xzvf yasm-1.3.0.tar.gz
 echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>4.2 The yasm decompression Success.>>>>>>>>>>>>>>>>>>>>>"
 cd yasm-1.3.0
 echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>4.3 The yasm build start>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-./configure --prefix="$HOME_DIRECTORY/ffmpeg_build" --bindir="$HOME_DIRECTORY/ffmpeg_bin"
+./configure --prefix="$HOME_DIRECTORY/ffmpeg_build" --bindir="$HOME_DIRECTORY/bin"
 sudo make -j4
 sudo make install
 echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>4.4 The yasm build finish.>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
@@ -121,38 +119,38 @@ echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>5.4 The pkg-config build finish.>>>>>>>>>>>>>
 fi
 echo "-------------------------------------------------------------------------------------"
 
+# install the x264.pc
+#sudo touch $HOME_DIRECTORY/ffmpeg_build/lib/pkgconfig/x264.pc
+
+#echo "prefix=$HOME_DIRECTORY/ffmpeg_build
+#exec_prefix=${prefix}
+#libdir=${exec_prefix}/lib
+#includedir=${prefix}/include
+#Name: x264
+#Description: H.264 (MPEG AVC) encoder library
+#Version: 0.164.x
+#Libs: -L${exec_prefix}/lib -lx264 -lpthread -lm -ldl
+#Libs.private:
+#Cflags: -I${prefix}/include" > $HOME_DIRECTORY/ffmpeg_build/lib/pkgconfig/x264.pc
+# Test the x264 install process
+#cat $HOME_DIRECTORY/ffmpeg_build/lib/pkgconfig/x264.pc
+#echo "-----------------------------X264.PC CREATE SUCCESS----------------------------------"
 # 8. Add x264
-echo -e "\033[5;34m-----------------------------INSTALL X264--------------------------------------------\033[0m"
+echo -e "\033[5;33m-----------------------------INSTALL X264--------------------------------------------\033[0m"
 if [ $(pwd) != $HOME_DIRECTORY/ffmpeg_sources ];then
 cd $HOME_DIRECTORY/ffmpeg_sources
 git clone --depth 1 https://code.videolan.org/videolan/x264.git
-echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>6.1 The x264 download Success.>>>>>>>>>>>>>>>>>>>>"
+echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>7.1 The x264 download Success.>>>>>>>>>>>>>>>>>>>>"
 cd x264
-PKG_CONFIG_PATH="$HOME_DIRECTORY/ffmpeg_build/lib/pkgconfig"
-./configure --prefix="$HOME_DIRECTORY/ffmpeg_build" --bindir="$HOME_DIRECTORY/ffmpeg_bin" --enable-static
-echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>6.2 The x264 build start>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+PKG_CONFIG_PATH="$HOME_DIRECTORY/ffmpeg_build/lib/pkgconfig" && \
+./configure --prefix="$HOME_DIRECTORY/ffmpeg_build" --bindir="$HOME_DIRECTORY/bin" --enable-static --disable-asm
+echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>7.2 The x264 build start>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 sudo make -j4
 sudo make install
-echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>6.3 The x264 build finish.>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>7.3 The x264 build finish.>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 fi
 echo "-------------------------------------------------------------------------------------"
 
-# install the x264.pc
-sudo touch $HOME_DIRECTORY/ffmpeg_build/lib/pkgconfig/x264.pc
-
-echo "prefix=$HOME_DIRECTORY/ffmpeg_build
-exec_prefix=${prefix}
-libdir=${exec_prefix}/lib
-includedir=${prefix}/include
-Name: x264
-Description: H.264 (MPEG AVC) encoder library
-Version: 0.164.x
-Libs: -L${exec_prefix}/lib -lx264 -lpthread -lm -ldl
-Libs.private:
-Cflags: -I${prefix}/include" > x264.pc
-# Test the x264 install process
-cat x264.pc
-echo "-----------------------------X264.PC CREATE SUCCESS----------------------------------"
 echo -e "\033[5;34m-----------------------------INSTALL X265--------------------------------------------\033[0m"
 
 if [ $(pwd) != $HOME_DIRECTORY/ffmpeg_sources ];then
@@ -184,6 +182,10 @@ Cflags: -I${includedir}" > x265.pc
 # Test the x265 install process
 cat x265.pc
 echo "-----------------------------X265.PC CREATE SUCCESS---------------------------------"
+
+
+
+
 echo -e "\033[5;34m-----------------------------INSTALL AAC--------------------------------------------\033[0m"
 if [ $(pwd) != $HOME_DIRECTORY/ffmpeg_sources ];then
 cd $HOME_DIRECTORY/ffmpeg_sources
@@ -201,19 +203,21 @@ sudo make -j4
 sudo make install
 fi
 echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>7.3 The AAC build finish.>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo -e "\033[5;34m-----------------------------INSTALL OPUS-------------------------------------------\033[0m"
-if [ $(pwd) != $HOME_DIRECTORY/ffmpeg_sources ];then
-cd $HOME_DIRECTORY/ffmpeg_sources
-curl -O -L https://archive.mozilla.org/pub/opus/opus-1.3.1.tar.gz
-echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>8.1 The OPUS download Success.>>>>>>>>>>>>>>>>>>>>>>>>>>"
-tar xzvf opus-1.3.1.tar.gz
-cd opus-1.3.1
-echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>8.2 The OPUS build start>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-./configure --prefix="$HOME_DIRECTORY/ffmpeg_build" --disable-shared
-sudo make -j4
-sudo make install
-echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>8.3 The OPUS build finish.>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-fi
+
+#echo -e "\033[5;34m-----------------------------INSTALL OPUS-------------------------------------------\033[0m"
+#if [ $(pwd) != $HOME_DIRECTORY/ffmpeg_sources ];then
+#cd $HOME_DIRECTORY/ffmpeg_sources
+# curl -O -L https://archive.mozilla.org/pub/opus/opus-1.3.1.tar.gz
+#echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>8.1 The OPUS download Success.>>>>>>>>>>>>>>>>>>>>>>>>>>"
+#tar xzvf opus-1.3.1.tar.gz
+#cd opus-1.3.1
+#echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>8.2 The OPUS build start>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+#./configure --prefix="$HOME_DIRECTORY/ffmpeg_build" --disable-shared
+#sudo make -j4
+#sudo make install
+#echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>8.3 The OPUS build finish.>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+#fi
+
 echo -e "\033[5;34m-----------------------------INSTALL FFmpeg------------------------------------------\033[0m"
 if [ $(pwd) != $HOME_DIRECTORY/ffmpeg_sources ];then
 cd $HOME_DIRECTORY/ffmpeg_sources
@@ -221,17 +225,17 @@ cd $HOME_DIRECTORY/ffmpeg_sources
 curl -O -L https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
 tar xjvf ffmpeg-snapshot.tar.bz2
 cd ffmpeg
-PATH="$HOME_DIRECTORY/ffmpeg_bin:$PATH" PKG_CONFIG_PATH="$HOME_DIRECTORY/ffmpeg_build/lib/pkgconfig" ./configure \
+PATH="$HOME_DIRECTORY/bin:$PATH" PKG_CONFIG_PATH="$HOME_DIRECTORY/ffmpeg_build/lib/pkgconfig" ./configure \
   --prefix="$HOME_DIRECTORY/ffmpeg_build" \
   --pkg-config-flags="--static" \
   --extra-cflags="-I$HOME_DIRECTORY/ffmpeg_build/include" \
   --extra-ldflags="-L$HOME_DIRECTORY/ffmpeg_build/lib" \
   --extra-libs=-lpthread \
   --extra-libs=-lm \
-  --bindir="$HOME_DIRECTORY/ffmpeg_bin" \
+  --bindir="$HOME_DIRECTORY/bin" \
   --enable-gpl \
   --enable-libfdk_aac \
-  --enable-libopus \
+ # --enable-libopus \
   --enable-libx264 \
   --enable-libx265 \
   --enable-nonfree
